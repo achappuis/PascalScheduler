92cb475bceedf0e24e2f040e2feb71d9cf5c19a0
Aurélien Chappuis <aurelienchappuis@ymail.com>
2015-03-21 23:10:29 +0100

    Use hex values.

--- src/init.s
+++ src/init.s
@@ -4,16 +4,16 @@ Copyright Aurélien Chappuis (2014)
 aurelienchappuis@ymail.com
 
 This software is governed by the CeCILL-B license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
+abiding by the rules of distribution of free software.  You can  use,
 modify and/ or redistribute the software under the terms of the CeCILL-B
 license as circulated by CEA, CNRS and INRIA at the following URL
-"http://www.cecill.info". 
+"http://www.cecill.info".
 
 As a counterpart to the access to the source code and  rights to copy,
 modify and redistribute granted by the license, users are provided only
 with a limited warranty  and the software's author,  the holder of the
 economic rights,  and the successive licensors  have only  limited
-liability. 
+liability.
 
 In this respect, the user's attention is drawn to the risks associated
 with loading,  using,  modifying and/or developing or reproducing the
@@ -22,16 +22,15 @@ that may mean  that it is complicated to manipulate,  and  that  also
 therefore means  that it is reserved for developers  and  experienced
 professionals having in-depth computer knowledge. Users are therefore
 encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
 
 The fact that you are presently reading this means that you have had
 knowledge of the CeCILL-B license and that you accept its terms.
 */
 
 	.cpu cortex-m0
-
 
   .equ  THREAD_MODE_MPS, 0x0
   .equ  THREAD_MODE_PSP, 0x2
@@ -43,12 +42,12 @@ knowledge of the CeCILL-B license and that you accept its terms.
 	.section	.vectors
 	@ .section	.vectors,"aw",%progbits
 	.align	2
-	.word	MAIN_STACK_TOP 
-	.word	_start 
-	.word	0 
+	.word	MAIN_STACK_TOP
+	.word	_start
 	.word	0
-	.word	-1
-	.word	0x000000ff
+	.word	0
+	.long   0x80000100
+	.long   0x80000000
 
   @ This section is loaded in Flash, but the relocation is done in RAM at 0x20000000
 	.section	.remapped_vectors, "ax"
@@ -65,21 +64,21 @@ _vectors:
   .long 0
 	@ Hard Fault
   ldr r0, =handler_hardfault
-  mov pc,r0	 
-   
-  .long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	@ SVCall 
+  mov pc,r0
+
+  .long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	@ SVCall
   ldr r0,=handler_svc
   mov pc,r0
 
-	.long 0 
-	.long 0 
+	.long 0
+	.long 0
 	.long 0
 
 	@ SysTick
@@ -87,36 +86,36 @@ _vectors:
   mov pc, r0
 
 	.long 0                 @ IRQ0
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-  .long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
-	.long 0 
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+  .long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
+	.long 0
 
 	.text
 	.code	16
@@ -126,14 +125,14 @@ _vectors:
   Function: init
 
   This assembly function is the entry point of the system after reset.
-  
+
   It initialise BSS, Heap and Stack before running any C code.
 
   See Also:
   - <_main>
-  - <c_init> 
+  - <c_init>
 */
-	.global _start	
+	.global _start
 	.func 	_start
 _start:
   @ Copy Data from flash to his relocation address
@@ -147,7 +146,7 @@ vector_loop:
   ADD     r1, r1, #4
   ADD     r3, r3, #4
   SUB     r5, #1
-  BNE     vector_loop 
+  BNE     vector_loop
 
   @ Initialise BSS
   LDR     r3, =(__bss_start)
