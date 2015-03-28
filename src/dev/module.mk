c240b3be2a11a33fb49a2804143d054c95d6186a
Aurélien Chappuis <aurelienchappuis@ymail.com>
2015-03-28 18:52:04 +0100

    Add MMC and SPI to dev/module.mk

--- src/dev/module.mk
+++ src/dev/module.mk
@@ -2,5 +2,7 @@ include src/dev/gpio/module.mk
 
 include src/dev/i2c/module.mk
 include src/dev/lcd/module.mk
-include src/dev/uart/module.mk
+include src/dev/mmc/module.mk
 include src/dev/pwm/module.mk
+include src/dev/spi/module.mk
+include src/dev/uart/module.mk
