96abdc11bdf1949bde7992d150ebd1118fa3a409
Aurélien Chappuis <aurelienchappuis@ymail.com>
2015-03-30 20:37:34 +0200

    Improve size calculation, and use UNSTUFF_BITS macro from previous commit (imported from linux)

--- src/dev/mmc/mmc_spi.c
+++ src/dev/mmc/mmc_spi.c
@@ -60,6 +60,20 @@ mmc_crc7(uint8_t *data,uint8_t length)
     return (crc << 1) | 0x1;
 }
 
+void
+swap_words(uint8_t *data)
+{
+    uint8_t tmp;
+
+    tmp = data[3];
+    data[3] = data[0];
+    data[0] = tmp;
+
+    tmp = data[2];
+    data[2] = data[1];
+    data[1] = tmp;
+}
+
 static void
 mmc_send(uint8_t *buf, uint8_t size) {
     dev_write(buf, size, &spi_node);
@@ -153,15 +167,31 @@ static int
 mmc_read_sector(struct mmc_info *mmc_info, uint8_t *buf, int sector) {
     uint8_t crc[2];
 
-    mmc_send_cmd(MMC_CMD17, sector * mmc_info->csd.read_bl_len);
+    mmc_send_cmd(MMC_CMD17, sector * mmc_info->read_block_size);
     if (mmc_spi_read_status() != 0)
         return MMC_ERROR;
 
     if (mmc_spi_wait_start_block(0) == MMC_ERROR)
         return MMC_ERROR;
 
-    mmc_read(buf, mmc_info->csd.read_bl_len); // sector
+    mmc_read(buf, mmc_info->read_block_size); // sector
     mmc_read(crc, 2);   // CRC
+
+    return MMC_OK;
+}
+
+static int
+mmc_write_sector(struct mmc_info *mmc_info, uint8_t *buf, int sector) {
+    uint8_t head[2];
+    head[0] = 0xFF;
+    head[1] = 0xFE;
+
+    mmc_send_cmd(MMC_CMD24, sector * mmc_info->csd.write_bl_len);
+    if (mmc_spi_read_status() != 0)
+        return MMC_ERROR;
+
+    mmc_send(head, 2);
+    mmc_send(buf, mmc_info->csd.write_bl_len);
 
     return MMC_OK;
 }
@@ -172,34 +202,78 @@ mmc_read_sector(struct mmc_info *mmc_info, uint8_t *buf, int sector) {
 #define mmc_read_r7(_buf) mmc_read((_buf), 5);
 
 static int
-mmc_read_csd(struct mmc_info *mmc_info)
+sd_decode_csd(uint32_t *buf, struct mmc_info *mmc_info)
 {
     struct mmc_csd *mmc_csd = &(mmc_info->csd);
-    uint8_t buf[18]; //16 byte + 2 CRC byte
 
-    mmc_info->mem_size = 0;
+    mmc_csd->c_structure = UNSTUFF_BITS(buf,126,2);
+    mmc_csd->read_bl_len = UNSTUFF_BITS(buf,80,4);
+    mmc_csd->write_bl_len = UNSTUFF_BITS(buf,22,4);
+    mmc_info->read_block_size = 1 << mmc_csd->read_bl_len;
+    mmc_info->write_block_size = 1 << mmc_csd->write_bl_len;
 
-    mmc_send_cmd(MMC_CMD09, 0);
-    mmc_spi_wait_start_block(1);
-    mmc_read(buf, 18);
-    if (mmc_crc7(buf, 15) != buf[15]) return MMC_ERROR;
-
-    mmc_csd->c_size=0;mmc_csd->c_mult=0;mmc_csd->read_bl_len=0;
-    // For Some reason some cards seems no to comply any of this CSD.
-    if ((buf[0] & 0xC0) == 0x00) { // CSD Version 1
-        mmc_csd->read_bl_len = (buf[5] & 0x0F);
-        mmc_csd->c_size      = (buf[5] & 0x0F);
-        mmc_csd->c_mult      = ((buf[10] & 0x03) << 1) | ((buf[11] & 0x8) >> 7);
-        mmc_info->mem_size = 0;
-    } else if ((buf[0] & 0xC0) == 0x40) { // CSD Version 2
-        mmc_csd->c_size = (uint32_t)buf[9] | ((uint32_t)buf[8] << 8) | (((uint32_t)buf[7] & 0x1F) << 16);
-        mmc_info->mem_size = (mmc_csd->c_size+1 * 512);// Size in Mib
+    if (mmc_csd->c_structure == 0) { // CSD Version 1
+        mmc_csd->c_size = UNSTUFF_BITS(buf,62,12);
+        mmc_csd->c_mult = UNSTUFF_BITS(buf,47,3);
+        int mult     = 1 << (mmc_csd->c_mult + 2);
+        int blocknr  = (mmc_csd->c_size + 1) * mult;
+        mmc_info->mem_size = blocknr * mmc_info->read_block_size / 1024 / 1024;
+    } else if (mmc_csd->c_structure == 1) { // CSD Version 2
+        mmc_csd->c_size = UNSTUFF_BITS(buf,48,22);
+        mmc_info->mem_size = (mmc_csd->c_size+1 * 512);
+    } else {
+        return MMC_ERROR;
     }
 
-    mmc_info->csd.read_bl_len = 512;
-    mmc_info->csd.write_bl_len = 512;
+    return MMC_OK;
+}
+
+static int
+mmc_decode_csd(uint32_t *buf, struct mmc_info *mmc_info)
+{
+    struct mmc_csd *mmc_csd = &(mmc_info->csd);
+
+    mmc_csd->c_structure = UNSTUFF_BITS(buf,126,2);
+    mmc_csd->read_bl_len = UNSTUFF_BITS(buf,80,4);
+    mmc_csd->write_bl_len = UNSTUFF_BITS(buf,22,4);
+    mmc_info->read_block_size = 1 << mmc_csd->read_bl_len;
+    mmc_info->write_block_size = 1 << mmc_csd->write_bl_len;
+
+// CSD Version 1.0
+// CSD Version 1.1
+// CSD Version 1.2
+    mmc_csd->c_size = UNSTUFF_BITS(buf,62,12);
+    mmc_csd->c_mult = UNSTUFF_BITS(buf,47,3);
+    int mult     = 1 << (mmc_csd->c_mult + 2);
+    int blocknr  = (mmc_csd->c_size + 1) * mult;
+    mmc_info->mem_size = blocknr * mmc_info->read_block_size / 1024 / 1024;
 
     return MMC_OK;
+}
+
+static int
+mmc_read_csd(struct mmc_info *mmc_info)
+{
+    uint32_t buf[5]; //16 byte + 2 CRC byte
+    uint32_t i;
+    
+    mmc_send_cmd(MMC_CMD09, 0);
+    mmc_spi_wait_start_block(1);
+    mmc_read((uint8_t*)buf, 18);
+    if (mmc_crc7((uint8_t*)buf, 15) != ((uint8_t*)buf)[15]) return MMC_ERROR;
+
+    for (i = 0;i < 4;i++)
+        swap_words((uint8_t*)&buf[i]);
+
+
+    if (UNSTUFF_BITS(buf,122,4) == 0) {
+        printf("MMC : MMC 1.0 or SD, opting for the SD option\n");// there is probably a better option!
+        return sd_decode_csd(buf, mmc_info);
+    } else {
+        return mmc_decode_csd(buf, mmc_info);
+    }
+
+    return MMC_ERROR;
 }
 
 static int
@@ -245,7 +319,7 @@ mmc_get_info(struct mmc_info *mmc_info)
     }
     if(((mmc_ocr >> 30) & 0x01) != 0) {
         printf("MMC : Memory Card is a SDHC!\n");
-        mmc_info->flags |= MMC_SDHC;
+        mmc_info->flags |= SD_SDHC;
     }
 
     /*
@@ -374,20 +448,42 @@ mmc_spi_close(struct vnode *vnode)
 int
 mmc_spi_write(struct vnode *vnode, struct uio *uio)
 {
-    __UNUSED(uio);
+  __UNUSED(uio);
 
-    struct mmc_info *mmc_info = (struct mmc_info *)vnode->data;
-    if (mmc_info == NULL)
-        return -1;
+  struct mmc_info *mmc_info = (struct mmc_info *)vnode->data;
+  int i;
+  uint8_t *ptr;
+  uint8_t buf[MAX_BLOCK_LENGTH];
 
-    return 0;
+  if (mmc_info == NULL)
+      return -1;
+  if (uio->uio_rw != UIO_WRITE)
+      return -1;
+
+  uint32_t read_sector = vnode->offset / mmc_info->read_block_size;
+  uint32_t read_offset = vnode->offset % mmc_info->read_block_size;
+  i = read_offset;
+
+  if ((uio->uio_resid+read_offset) > mmc_info->read_block_size)
+      return -1;
+
+  if (mmc_read_sector(mmc_info, buf, read_sector) != MMC_OK)
+      return -1;
+
+  ptr = uio->uio_iov->iov_base;
+  for(; uio->uio_resid > 0; uio->uio_resid--, ptr++) {
+      buf[i++] = *ptr;
+  }
+
+  if (mmc_write_sector(mmc_info, buf, read_sector) != MMC_OK)
+      return -1;
+
+  return 0;
 }
 
 int
 mmc_spi_read(struct vnode *vnode, struct uio *uio)
 {
-    __UNUSED(uio);
-
     struct mmc_info *mmc_info = (struct mmc_info *)vnode->data;
     int i;
     uint8_t *ptr;
@@ -398,11 +494,11 @@ mmc_spi_read(struct vnode *vnode, struct uio *uio)
     if (uio->uio_rw != UIO_READ)
         return -1;
 
-    uint32_t read_sector = vnode->offset / mmc_info->csd.read_bl_len;
-    uint32_t read_offset = vnode->offset % mmc_info->csd.read_bl_len;
+    uint32_t read_sector = vnode->offset / mmc_info->read_block_size;
+    uint32_t read_offset = vnode->offset % mmc_info->read_block_size;
     i = read_offset;
 
-    if ((uio->uio_resid+read_offset) > mmc_info->csd.read_bl_len)
+    if ((uio->uio_resid+read_offset) > mmc_info->read_block_size)
         return -1;
 
     if (mmc_read_sector(mmc_info, buf, read_sector) != MMC_OK)
